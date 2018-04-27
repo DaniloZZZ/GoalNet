641e7a7a89c4dab3d7647cfa6dc972e6f868456e
DaniloZZZ <lkv97dn@gmail.com>
2018-04-28 01:28:39 +0300

    sched bot

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,7 @@
 import config
 
 import tgflow as tgf
+import sched,time
 
 #from modules import login, student, apply
 from UI import goals, notifs
@@ -8,10 +9,21 @@ from DataBase import db
 
 
 tgf.configure(token=config.token,state='start')
+def set_time_sec(sec):
+    s = sched.scheduler(time.time, time.sleep)
+    def sch(i):
+        def send():
+            tgf.send_state('notif',i.chat.id)
+        s.enter(sec,1,send)
+        s.run()
+    return sch
 
 UI ={
     'start':{
-        't':'hello there!'
+        't':'hello there!',
+        'b':[
+            {'call del':tgf.action(set_time_sec(10))}
+        ]
     }
 }
 UI.update(goals.UI)
