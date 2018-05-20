155ce7f92d2a95cb7f7148e368656eb0bac058d0
Your Name <you@example.com>
2018-05-20 14:38:27 +0000

    added vanilia bot

--- bot/main.py
+++ bot/main.py
@@ -1,35 +1,67 @@
 import config
-
+import log
 import tgflow as tgf
-import sched,time
+import schedule, time
+import threading
 
 #from modules import login, student, apply
 from UI import goals, notifs
 from DataBase import db
 
+lg=log.bot.info
 
 tgf.configure(token=config.token,state='start')
-def set_time_sec(sec):
-    s = sched.scheduler(time.time, time.sleep)
-    def sch(i):
-        def send():
-            print('sending',sec)
-            tgf.send_state('notif',i.message.chat.id)
-        s.enter(sec,1,send)
-        print('starting',sec)
-        s.run()
-    return sch
+def sch(i):
+	def send():
+		print('sending')
+		tgf.send_state('notif',i.chat.id)
+	#t = threading.Timer(sec, send)
+	#t.start()
+	#s.enter(sec,1,send)
+	#s.run(blocking=False)
+	lg('exit sch')
+	return send
+
+def set_sc(i):
+	lg('sceduling every %i min'%int(i.text))
+	j = schedule.every(int(i.text)).minutes.do(sch(i))
+	return 'cancel', {'time':int(i.text), 'job':j}
+
+def cancel(d):
+	lg("cancelling. data",d)
+	schedule.cancel_job(d.get('job'))
+	lg("gobs running",schedule.jobs)
+	return 'start'
 
 UI ={
     'start':{
         't':'hello there!',
         'b':[
-            {'call del':tgf.action(set_time_sec(10))}
+            {'create sch':lambda i: 'get_time'}
         ]
-    }
+    },
+	'get_time':{
+		't':'send me minutes interval',
+		'react':tgf.action(set_sc,react_to='text')
+	},
+	'cancel':{
+			't':tgf.paste('cancel sched %i','time'),
+			'b':[
+					{'cancel':tgf.action(cancel)}
+			]
+
+}
+
 }
 UI.update(goals.UI)
 UI.update(notifs.UI)
+
+def runs():
+	while True:
+		schedule.run_pending()
+		time.sleep(1)
+t = threading.Timer(1,runs)
+t.start()
 
 tgf.start(UI)
 
