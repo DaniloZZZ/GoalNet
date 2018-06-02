bb5126951b4f04e51d03522123bb7aa1c88f6c85
Danilozzz <lkv97dn@gmail.com>
2018-06-03 02:08:59 +0300

    fix not imported home in bot

--- bot/main.py
+++ bot/main.py
@@ -2,14 +2,14 @@ import config
 import log
 import tgflow as tgf
 import threading
-from UI import goals, notifs,login, misc
+from UI import goals, notifs,login, misc,home
 from StateMachine import States
 from DataBase import db
 
 from telebot import apihelper
 
-apihelper.proxy = {
-    'https':'socks5://localhost:9050'}
+#apihelper.proxy = {
+    #'https':'socks5://localhost:9050'}
 #apihelper.CONNECT_TIMEOUT=20
 
 lg=log.bot.info
@@ -19,10 +19,12 @@ tgf.configure(token=config.token,state=States.HOME)
 UI = {}
 print(misc.UI)
 UI.update(misc.UI)
+UI.update(home.UI)
 UI.update(login.UI)
 UI.update(goals.UI)
 UI.update(notifs.UI)
 
+print(UI)
 tgf.start(UI)
 
 
