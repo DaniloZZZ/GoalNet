c50db40f760864b7d3339e72d500d1d4b2b3a0f5
Danil <lkv97dn@gmail.com>
2019-02-14 02:26:30 +0300

    Telegram bot works

--- v2/connectors/telegram/main.py
+++ v2/connectors/telegram/main.py
@@ -7,6 +7,7 @@ from enum import Enum
 
 from network import ConnectorNetwork
 from utils__ import  get_network_config
+from database import DB
 
 def _print(*args):
     print(">telegram bot>",*args)
@@ -14,22 +15,40 @@ def _print(*args):
 class States(Enum):
     action=1
     settings=2
-
+    login=3
+    start=4
 
 def bot(netconf):
     net = ConnectorNetwork(netconf,
             appid='0',
             name='telegram'
             )
+    db = DB()
+
     def handle_notif(notif):
         str_notif = json.dumps(notif)
+        user_id = notif['user_id']
+        tgid = db.get_tg_id(user_id)
         _print("got notif:",str_notif)
-        tgf.send_raw(json.dumps(str_notif),0)
+        message = "Got new notif of type %s. Content: %s"%(
+                notif.get('type'),notif.get('content')
+                )
+        tgf.send_raw(message, tgid)
         return 'OK'
 
     net.listen_for_notif(handle_notif)
 
-    def handle_action(i):
+    def login_uid_1(i):
+        telegram_id = i.message.chat.id
+        user_id = '1'
+        db.save_tg_id(user_id,telegram_id)
+
+        return States.action, {'user_id': user_id}
+
+    def handle_action(i,user_id=None):
+        if not user_id:
+            _print('user not logged in')
+            return States.login
         text = i.text
         msg_type = 'telegram'
         try:
@@ -39,10 +58,11 @@ def bot(netconf):
         message = {
                 'type':msg_type,
                 'content':content,
-                'user_id':'10',
+                'user_id':user_id,
                 }
         net.send(message)
-        return States.action
+        # stay silent
+        return -1
 
     UI =  {
             States.action:{
@@ -57,10 +77,28 @@ def bot(netconf):
                 'b':[
                     {"Action":tgf.action(States.action)}
                     ],
+                },
+            States.start:{
+                't':'Welcome!',
+                'b':[
+                    {"Log in":tgf.action(States.login)},
+                    ]
+                },
+            States.login:{
+                't':'Please log in',
+                'b':[
+                    {"Log in as 1":
+                        tgf.action(login_uid_1)}
+                    ],
                 }
             }
+    key='539066078:AAHCUsr8ZoP9JtP5KqOMuL7f_UoFyyH6wik'
 
-    tgf.configure(token="0",state=States.action,apiModel=cliAPI)
+    tgf.configure(token=key,
+            state=States.start,
+            #apiModel=cliAPI,
+            verbose=True,
+            )
     tgf.start(UI)
 
 def main():
