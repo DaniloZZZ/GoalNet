68c951d641d0b83cf471c45cc4251fc293b1c605
Danil <lkv97dn@gmail.com>
2019-02-14 04:51:30 +0300

    telegram node launch script

---
+++ v2/connectors/telegram/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo "starting telegram bot connector"
+
+UTILS=utils__.py
+GLOBAL_UTILS=/home/danlkv/GoalNet/v2/utils.py
+
+if [ ! -e $UTILS ]; then
+    echo "No utils file, linking from $GLOBAL_UTILS"
+    ln -s $GLOBAL_UTILS $UTILS
+fi
+
+python3 main.py
+
